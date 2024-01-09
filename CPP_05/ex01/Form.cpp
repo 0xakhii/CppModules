743a717824316d1f323d0b349227b6fc3fc947a5
0xakhii <ojamal@student.1337.ma>
2024-01-09 17:10:16 +0100

    modified:   CPP_05/ex01/Form.cpp 	modified:   CPP_05/ex01/Form.hpp

--- CPP_05/ex01/Form.cpp
+++ CPP_05/ex01/Form.cpp
@@ -3,26 +3,26 @@
 Form::Form(): _name("default"), _signed(false), signGrade(), execGrade(){}
 
 Form::Form(std::string const name, int const signGrade, int const execGrade): _name(name), signGrade(signGrade), execGrade(execGrade){
-    if (signGrade < 1 || execGrade < 1)
-        throw Form::GradeTooHighException();
-    else if (signGrade > 150 || execGrade > 150)
-        throw Form::GradeTooLowException();
-    else
-        _signed = false;
+	if (signGrade < 1 || execGrade < 1)
+		throw Form::GradeTooHighException();
+	else if (signGrade > 150 || execGrade > 150)
+		throw Form::GradeTooLowException();
+	else
+		_signed = false;
 }
 
 Form::~Form(){}
 
 std::string const Form::getName() const {
-    return _name;
+	return _name;
 }
 
 int   Form::getSignGrade(){
-    return signGrade;
+	return signGrade;
 }
 
 int   Form::getExecGrade(){
-    return execGrade;
+	return execGrade;
 }
 
 const char* Form::GradeTooHighException::what() const throw(){
@@ -34,6 +34,6 @@ const char* Form::GradeTooLowException::what() const throw(){
 }
 
 std::ostream &operator<<(std::ostream &out, Form &form){
-    out << form.getName() << ", sign grade: " << form.getSignGrade() << ", exec grade: " << form.getExecGrade() << std::endl;
-    return out;
+	out << form.getName() << ", sign grade: " << form.getSignGrade() << ", exec grade: " << form.getExecGrade() << std::endl;
+	return out;
 }
--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -3,19 +3,19 @@
 #include "Bureaucrat.hpp"
 
 class Form{
-    private:
-        std::string const _name;
-        bool _signed;
-        int const signGrade;
-        int const execGrade;
-    public:
-        Form();
-        Form(std::string const _name, int const signGrade, int const execGrade);
-        ~Form();
-        std::string const getName() const;
-        int  getSignGrade();
-        int  getExecGrade();
-        class	GradeTooHighException : public std::exception{
+	private:
+		std::string const _name;
+		bool _signed;
+		int const signGrade;
+		int const execGrade;
+	public:
+		Form();
+		Form(std::string const _name, int const signGrade, int const execGrade);
+		~Form();
+		std::string const getName() const;
+		int  getSignGrade();
+		int  getExecGrade();
+		class	GradeTooHighException : public std::exception{
 			const char* what() const throw();
 		};
 		class	GradeTooLowException : public std::exception{
