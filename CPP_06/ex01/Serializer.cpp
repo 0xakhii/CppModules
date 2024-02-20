4f94ee393e552a3999cc3ad84a472c785242c8f3
0xakhii <ojamal@student.1337.ma>
2024-02-20 09:37:29 +0100

    Add Serializer class and implement serialization and deserialization functions

---
+++ CPP_06/ex01/Serializer.cpp
@@ -0,0 +1,22 @@
+#include "Serializer.hpp"
+
+Serializer::Serializer(){}
+
+Serializer::Serializer(Serializer const &copy){
+	(void)&copy;
+}
+
+Serializer &Serializer::operator=(Serializer const &serialize){
+	(void)&serialize;
+	return (*this);
+}
+
+Serializer::~Serializer(){}
+
+uintptr_t Serializer::serialize(Data* ptr){
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data*	Serializer::deserialize(uintptr_t raw){
+	return (reinterpret_cast<Data*>(raw));
+}
--- CPP_06/ex01/Serializer.hpp
+++ CPP_06/ex01/Serializer.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include "iostream"
+#include <stdint.h>
 
-class Serializer
-{
+struct Data{
+	int	a;
+};
+
+class Serializer{
 	public:
 		Serializer();
+		Serializer(Serializer const &copy);
+		Serializer &operator=(Serializer const &serialize);
 		~Serializer();
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
 };
