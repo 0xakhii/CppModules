6839cd0a1f45193a27edaf606f42ba253dd172b6
0xakhii <ojamal@student.1337.ma>
2024-02-21 02:49:58 +0100

    Add Base class and related functions and files

---
+++ CPP_06/ex02/Base.cpp
@@ -0,0 +1,15 @@
+#include "Base.hpp"
+
+Base::~Base(){}
+
+Base* generate(){
+
+}
+
+void identify(Base* p){
+
+}
+
+void identify(Base& p){
+
+}
---
+++ CPP_06/ex02/Main.cpp
@@ -0,0 +1,5 @@
+#include "Base.hpp"
+
+int main(){
+	
+}
