f7e5ad8a90ce465a0a78c300c1a4c2ecfee09336
0xakhii <ojamal@student.1337.ma>
2024-02-21 07:06:14 +0100

    Add CPP_07 exercises 0 and 1

---
+++ CPP_07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void	swap(T &a, T &b){
+	T c;
+	c = a;
+	a = b;
+	b = c;
+}
+
+template <typename T>
+T	min(T a, T b){
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+T	max(T a, T b){
+	if (a > b)
+		return a;
+	else
+		return b;
+}
---
+++ CPP_07/ex01/iter.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+template <typename T, typename F>
+void	iter(T *add, int len, F func){
+
+}
