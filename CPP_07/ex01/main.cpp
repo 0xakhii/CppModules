b5b9588a9475b7e8a1fb45f10388f70a1ebb1aed
0xakhii <ojamal@student.1337.ma>
2024-02-22 13:01:35 +0100

    Add debug output to iter function

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -3,6 +3,8 @@
 int main(){
 	char str[] = "hello";
 	int arr[] = {0, 1, 2, 5, 6};
+	std::cout << "first array: " << std::endl;
 	::iter(str, sizeof(str)/sizeof(char), func<char>);
+	std::cout << "second array: " << std::endl;
 	::iter(arr, (sizeof(arr)/sizeof(*arr)), func<int>);
 }
