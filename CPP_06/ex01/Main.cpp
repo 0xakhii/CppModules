4547e51e387cbbff2ae1b63d5204cf3f79ba348a
0xakhii <ojamal@student.1337.ma>
2024-02-22 06:13:52 +0100

    Refactor Main.cpp to improve serialization process

--- CPP_06/ex01/Main.cpp
+++ CPP_06/ex01/Main.cpp
@@ -1,12 +1,11 @@
-<uintptr_t>#include "Serializer.hpp"
+#include "Serializer.hpp"
 
 
 int main(){
 	Data *ptr = new Data;
-	ptr->a = 5;
 	std::cout << "ptr value before serialize: " << ptr << std::endl;
 	uintptr_t raw =  Serializer::serialize(ptr);
-	std::cout << "raw value after deserialize: " << Serializer::deserialize(raw) << std::endl;
-	std::cout << "ptr value after serialize: " << raw << std::endl;
+	std::cout << "ptr value after deserialize: " << Serializer::deserialize(raw) << std::endl;
+	std::cout << "raw value after serialize: 0x" << std::hex << raw << std::endl;
 	delete ptr;
 }
