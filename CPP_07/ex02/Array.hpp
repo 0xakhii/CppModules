f8c6485a2aed1dfa08db662c3886a17ee2d10430
0xakhii <ojamal@student.1337.ma>
2024-02-24 00:26:53 +0100

    Refactor Array class implementation and add test cases

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -2,16 +2,40 @@
 
 #include <iostream>
 
-template <typename T> class Array{
+template<class T>class Array{
 	private:
-		T	elements;
+		T				*elements;
+		unsigned int	_size;
 	public:
-		Array(){
+		Array():elements(new T[0]), _size(0){}
+		Array(unsigned int n): elements(new T[n]), _size(n){}
+		Array(Array const &copy): elements(copy.elements), _size(copy._size){}
+		~Array(){
+			if (elements)
+				delete [] elements;
 		}
-		Array(unsigned int n){
+		Array &operator=(Array const &array){
+			if (this == &array)
+				return *this;
+			if (elements)
+				delete [] elements;
+			_size = array._size;
+			array.elements = new T[_size];
+			for (unsigned int i = 0; i <= _size; i++)
+				elements[i] = array.elements[i];
+			return *this;
 		}
-		Array(Array const &copy){
+		unsigned int size() const{
+			return this->_size;
 		}
-		Array &operator=(Array const &Array){
+		class IndexOutOfBounds : std::exception{
+			const char * what() const throw(){
+				return ("Index out of bounds");
+			}
+		};
+		T& operator[](long index){
+			if (index < 0 || _size <= index)
+				throw IndexOutOfBounds();
+			return elements[index];
 		}
 };
