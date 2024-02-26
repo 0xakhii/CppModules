6487e35785a5b6a27239168d81a436b3753f9f74
0xakhii <ojamal@student.1337.ma>
2024-02-26 17:25:19 +0100

    added more test cases for MutantStack class

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-# include <deque>
 # include <iostream> 
 # include <stack>
 # include <iterator>
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -1,28 +1,48 @@
 #include "MutantStack.hpp"
 
-int main()
-{
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+// int main()
+// {
+// 	MutantStack<int> mstack;
+// 	mstack.push(5);
+// 	mstack.push(17);
+// 	std::cout << mstack.top() << std::endl;
+// 	mstack.pop();
+// 	std::cout << mstack.size() << std::endl;
 
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	//[...]
+// 	mstack.push(0);
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::stack<int> s(mstack);
+// 	return 0;
+// }
+
+int main(){
+	MutantStack<int> _stack;
+	std::cout << "empty stack: " << (_stack.empty() ? "true" : "false") << std::endl;
+	for(int i = 0; i < 10; i++)
+		_stack.push(i);
+	std::cout << "empty stack: " << (_stack.empty() ? "true" : "false") << std::endl;
+	MutantStack<int>::iterator it = _stack.begin();
+	MutantStack<int>::iterator ite = _stack.end();
+	std::cout << "stack content: ";
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << ", ";
 		++it;
 	}
-	std::stack<int> s(mstack);
-	return 0;
+	std::cout << "\nstack size: " << _stack.size() << std::endl;
+	std::cout << "top element: " << _stack.top() << std::endl;
+	_stack.pop();
+	std::cout << "top element after pop: " << _stack.top() << std::endl;
 }
